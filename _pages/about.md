93fe7ffdb9925c672459d2aebda7d2b4ae668110
siddharth17196 <siddharth17196@iiitd.ac.in>
2020-07-25 16:48:43 +0530

    add

---
+++ _pages/about.md
@@ -0,0 +1,19 @@
+---
+permalink: /about/
+title: "About Me"
+layout : about
+last_modified_at: 2020-05-01T10:15:22-04:00
+toc: true
+---
+
+Hi, my name is Siddharth Nair and I’m a final-year Bachelor student in Electronics 
+and Communication at IIIT-Delhi.
+
+
+I enjoy sinking myself into deep learning projects and writing useless but cool scripts :/.  
+My interest areas are: machine/deep learning for object detection, protein sequence 
+prediction and autonomous vehicles.
+
+Feel free to connect [Github](https://github.com/siddharth17196), [Mail](mailto:siddharth17196@iiitd.ac.in), 
+[LinkedIn](https://www.linkedin.com/in/siddharthvinodnair/)
+
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Markup: Text Alignment",
+        "excerpt":"Default This is a paragraph. It should not have any alignment of any kind. It should just flow like you would normally expect. Nothing fancy. Just straight up text, free flowing, with love. Completely neutral and not picking a side or sitting on the fence. It just is. It just...","categories": ["Markup"],
+        "tags": ["alignment","content","css","markup"],
+        "url": "http://localhost:4000/markup/markup-text-alignment/",
+        "teaser": null
+      },{
+        "title": "Markup: Image Alignment",
+        "excerpt":"Welcome to image alignment! The best way to demonstrate the ebb and flow of the various image positioning options is to nestle them snuggly among an ocean of words. Grab a paddle and let’s get started. The image above happens to be centered. The rest of this paragraph is filler for...","categories": ["Markup"],
+        "tags": ["alignment","captions","content","css","image","markup"],
+        "url": "http://localhost:4000/markup/markup-image-alignment/",
+        "teaser": null
+      },{
+        "title": "Markup: HTML Tags and Formatting",
+        "excerpt":"A variety of common markup showing how the theme styles them. Header two Header three Header four Header five Header six Blockquotes Single line blockquote: Stay hungry. Stay foolish. Multi line blockquote with a cite reference: People think focus means saying yes to the thing you’ve got to focus on....","categories": ["Markup"],
+        "tags": ["content","css","formatting","html","markup"],
+        "url": "http://localhost:4000/markup/markup-html-tags-and-formatting/",
+        "teaser": "http://localhost:4000/assets/images/markup-syntax-highlighting-teaser.jpg"
+      },{
+        "title": "Markup: Another Post with Images",
+        "excerpt":"Here are some examples of what a post with images might look like. If you want to display two or three images next to each other responsively use figure with the appropriate class. Each instance of figure is auto-numbered and displayed in the caption. Figures (for images or video) One...","categories": [],
+        "tags": ["sample post","images","test"],
+        "url": "http://localhost:4000/markup-more-images/",
+        "teaser": "http://farm9.staticflickr.com/8426/7758832526_cc8f681e48_c.jpg"
+      },{
+        "title": "Markup: Syntax Highlighting",
+        "excerpt":"Syntax highlighting is a feature that displays source code, in different colors and fonts according to the category of terms. This feature facilitates writing in a structured language such as a programming language or a markup language as both structures and syntax errors are visually distinct. Highlighting does not affect...","categories": [],
+        "tags": ["code","syntax highlighting"],
+        "url": "http://localhost:4000/markup-syntax-highlighting/",
+        "teaser": "http://localhost:4000/assets/images/markup-syntax-highlighting-teaser.jpg"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["Jekyll"],
+        "tags": ["update"],
+        "url": "http://localhost:4000/jekyll/welcome-to-jekyll/",
+        "teaser": "http://localhost:4000/assets/images/500x300.png"
+      },{
+        "title": "Gemified Theme -- Alpha Release",
+        "excerpt":"Jekyll themes distributed as Ruby gems are finally here to make installing and upgrading much easier. Gone are the days of forking a repo just to “install it”. Or dealing with merge conflicts when pulling in upstream commits to “upgrade it”. If you’re interested in testing out Minimal Mistakes as...","categories": ["Jekyll"],
+        "tags": ["update"],
+        "url": "http://localhost:4000/jekyll/gemified-theme-alpha/",
+        "teaser": null
+      },{
+        "title": "Gemified Theme -- Beta Release",
+        "excerpt":"Hot on the heels of Jekyll v3.3.0 is a beta release of Minimal Mistakes… as a gemified theme. minimal-mistakes-jekyll can only be used with Jekyll proper. If you’re hosting on GitHub Pages or using that gem the theme won’t work. 3rd party themes haven’t been white-listed so it’s a no...","categories": ["Jekyll"],
+        "tags": ["update"],
+        "url": "http://localhost:4000/jekyll/gemified-theme-beta/",
+        "teaser": null
+      },{
+        "title": "Layout: Header Video",
+        "excerpt":"This post should display a header with a responsive video, if the theme supports it. Settings Parameter Required Description id Required ID of the video provider Required Hosting provider of the video, either youtube or vimeo YouTube To embed the following YouTube video at url https://www.youtube.com/watch?v=XsxDH4HcOWA (long version) or https://youtu.be/XsxDH4HcOWA...","categories": ["Layout","Uncategorized"],
+        "tags": ["video","layout"],
+        "url": "http://localhost:4000/layout/uncategorized/layout-header-video/",
+        "teaser": null
+      }]
